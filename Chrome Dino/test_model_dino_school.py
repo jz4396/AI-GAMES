93f66b2668cebd13bb334d90b6b68bba1ff6ac42
jz4396 <jz4396@gmail.com>
2017-05-10 17:51:55 -0400

    adding school size

---
+++ Chrome Dino/test_model_dino_school.py
@@ -0,0 +1,87 @@
+# test_model.py
+
+import numpy as np
+from grabscreen import grab_screen
+import cv2
+import time
+from directkeys import PressKey,ReleaseKey, W, A, S, D
+from alexdino import alexnet
+from getkeys import key_check
+
+import random
+
+SPACE = 0x39
+
+WIDTH = 200
+HEIGHT = 68
+LR = 1e-3
+EPOCHS = 10
+MODEL_NAME = 'pydino-fast-{1}-{1}-{1}-epochs-300K-data-School.model'.format(LR, 'alexnetv2',EPOCHS)
+
+t_time = 0.09
+
+def Jump():
+##    if random.randrange(4) == 2:
+##        ReleaseKey(W)
+##    else:
+    PressKey(SPACE)
+    
+def noJump():
+    ReleaseKey(SPACE)
+
+    
+model = alexnet(WIDTH, HEIGHT, LR)
+model.load(MODEL_NAME)
+
+def main():
+    last_time = time.time()
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
+    paused = False
+    while(True):
+        
+        if not paused:
+            # 800x600 windowed mode
+            screen = grab_screen(region=(380,100,980,240))
+            print('loop took {} seconds'.format(time.time()-last_time))
+            last_time = time.time()
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+            screen = cv2.resize(screen, (200, 68))
+
+            prediction = model.predict([screen.reshape(200,68,1)])[0]
+            print(prediction)
+
+            Jump_thresh = 0.93
+
+            if prediction[0] > Jump_thresh:
+                Jump()
+            else:
+                noJump()
+
+        keys = key_check()
+
+        # p pauses game and can get annoying.
+        if 'T' in keys:
+            if paused:
+                paused = False
+                time.sleep(1)
+            else:
+                paused = True
+                ReleaseKey(A)
+                ReleaseKey(W)
+                ReleaseKey(D)
+                time.sleep(1)
+
+main()       
+
+
+
+
+
+
+
+
+
+
